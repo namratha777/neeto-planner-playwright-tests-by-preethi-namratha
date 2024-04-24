91be1588d09a32b968c39db094114e1195144af2
namratha777 <preethinamratha777@gmail.com>
2024-04-24 11:17:07 +0530

    test suite for login

---
+++ e2e/tests/login.spec.ts
@@ -0,0 +1,9 @@
+// login.spec.ts
+
+import { test, expect } from "@playwright/test";
+
+test.describe("Login page", () => {
+  test("should login with the correct credentials", async ({ page }) => {
+
+  });
+});
