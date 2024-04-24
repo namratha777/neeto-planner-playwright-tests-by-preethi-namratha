62cc0a989e36d1fad2c2a2f8e3aee64aa86fcef3
namratha777 <preethinamratha777@gmail.com>
2024-04-24 16:33:14 +0530

    Added global setup and global teardown

--- e2e/tests/login.setup.ts
+++ e2e/tests/login.setup.ts
@@ -1,3 +1,50 @@
+import { test, expect } from "@playwright/test";
+import { faker } from "@faker-js/faker"
+import { LoginPage } from "/home/namratha77/neetoPlannerProject/playwright-tests/e2e/poms/login.ts";
+import { STORAGE_STATE } from "../../playwright.config";
+
+test.describe("Login page", () => {
+  test.setTimeout(120000);
+    let loginPage: LoginPage; // Declare loginPage outside of beforeEach
+
+    test.beforeEach(async ({ page }) => {
+        const newOTP = faker.string.numeric({ length: 6 });
+        // Initialize page objects
+        loginPage = new LoginPage(page); // Assign loginPage here
+        // Navigate to login page
+        //await page.goto("https://app.neetoauth.net/login?redirect_uri=neetoplanner.net");
+        await page.goto("/");
+    });
+
+    test("should login with the correct credentials", async ({ page }) => { // Remove newOTP from the test argument
+        // Test steps
+        await loginPage.enterEmail("cpts9gnqty9-planner-preethi_namratha-iiit_bhubaneshwar@bigbinary.com");
+
+        await expect(page.getByRole('heading', { name: 'Verify your email' })).toBeVisible();
+
+        // Use faker here to generate the OTP
+        const newOTP = faker.string.numeric({ length: 6 });
+
+        await loginPage.enterOTP(newOTP);
+
+        await expect(page.locator('[data-test-id="main-header"]')).toBeVisible({ timeout: 30000 });
+
+        await page.getByTestId('floating-action-menu-container').getByRole('button').click();
+        
+        await expect(page.getByRole('heading', { name: 'Preethi Neelap' })).toBeVisible();
+        await expect(page.getByRole('button', { name: 'Log out' })).toBeVisible();
+
+        await page.context().storageState({ path: STORAGE_STATE });
+    });
+});
+
+
+
+
+
+
+
+
 /*// login.spec.ts
 
 import { test, expect } from "@playwright/test"
@@ -34,6 +81,8 @@ test.describe("Login page", () => {
     });
 });
 */
+/*
+Iteration: 2
 import { test, expect } from "@playwright/test";
 import { faker } from "@faker-js/faker"
 import { LoginPage } from "/home/namratha77/neetoPlannerProject/playwright-tests/e2e/poms/login.ts";
@@ -47,7 +96,8 @@ test.describe("Login page", () => {
         // Initialize page objects
         loginPage = new LoginPage(page); // Assign loginPage here
         // Navigate to login page
-        await page.goto("https://app.neetoauth.net/login?redirect_uri=neetoplanner.net");
+        //await page.goto("https://app.neetoauth.net/login?redirect_uri=neetoplanner.net");
+        await page.goto("/");
     });
 
     test("should login with the correct credentials", async ({ page }) => { // Remove newOTP from the test argument
@@ -69,3 +119,4 @@ test.describe("Login page", () => {
         await expect(page.getByRole('button', { name: 'Log out' })).toBeVisible();
     });
 });
+*/
--- playwright.config.ts
+++ playwright.config.ts
@@ -1,43 +1,49 @@
 import { defineConfig, devices } from '@playwright/test';
-
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
+export const STORAGE_STATE = "./auth/session.json";
 export default defineConfig({
   testDir: './e2e',
-  /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    baseURL: "https://app.neetoauth.net/login?redirect_uri=neetoplanner.net",
   },
 
+
+  projects: [
+    {
+        name: "login",
+        testMatch: "**/login.setup.ts",
+    },
+    {
+      name: "teardown",
+      use: { ...devices["Desktop Chrome"] },
+      testMatch: "**/global.teardown.ts",
+    },
+    {
+        name: "Other tests",
+        dependencies: ["login"],
+        teardown: "teardown",
+        testMatch: "**/*.spec.ts",
+        testIgnore: "**/login.setup.ts",
+        use: {
+          storageState: STORAGE_STATE,
+      },
+    },
+],
+
   /* Configure projects for major browsers */
+  /*
   projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
 
-    /*
+    
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
@@ -68,7 +74,9 @@ export default defineConfig({
     //   name: 'Google Chrome',
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
-  ],
+  
+  
+
 
   /* Run your local dev server before starting the tests */
   // webServer: {
